1043779f724ded3705dded377067abe9f23bccaf
Luis <luiszu7779@gmail.com>
2025-07-07 04:44:40 +0200

    Mysql Docker dev

---
+++ prisma/migrations/20250707024426_init/migration.sql
@@ -0,0 +1,105 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `accounts` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `life` VARCHAR(255) NOT NULL,
+    `type` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `rib` VARCHAR(255) NOT NULL,
+    `balance` DOUBLE NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `budgets` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `life` VARCHAR(255) NOT NULL,
+    `year` BIGINT NOT NULL,
+    `month` BIGINT NOT NULL,
+    `type` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `amount` BIGINT NOT NULL,
+    `prelevement_day` BIGINT NULL,
+    `should_be_copied_next_month` BOOLEAN NOT NULL DEFAULT true,
+    `should_show_alert` BOOLEAN NOT NULL DEFAULT false,
+    `deleted_at` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `transactions` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `account_id` BIGINT NOT NULL,
+    `budget_id` BIGINT NULL,
+    `type` VARCHAR(255) NOT NULL,
+    `amount` DOUBLE NOT NULL,
+    `label` VARCHAR(255) NOT NULL,
+    `date` DATE NOT NULL,
+    `transfer_opposite_transaction_id` BIGINT NULL,
+    `deleted_at` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `stocks` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `code` VARCHAR(255) NOT NULL,
+    `exchange` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `last_closed_price` DOUBLE NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `portfolio_operations` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `transaction_id` BIGINT UNSIGNED NOT NULL,
+    `broker_fees_transaction_id` BIGINT NULL,
+    `stock_id` BIGINT UNSIGNED NOT NULL,
+    `quantity` BIGINT UNSIGNED NOT NULL DEFAULT 1,
+    `type` VARCHAR(255) NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `deleted_at` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `portfolio_performance_history` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `account_id` BIGINT NOT NULL,
+    `date` DATE NOT NULL,
+    `value` BIGINT NOT NULL,
+    `performance` BIGINT NOT NULL,
+    `deleted_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `transactions` ADD CONSTRAINT `transactions_account_id_fkey` FOREIGN KEY (`account_id`) REFERENCES `accounts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `transactions` ADD CONSTRAINT `transactions_budget_id_fkey` FOREIGN KEY (`budget_id`) REFERENCES `budgets`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `portfolio_operations` ADD CONSTRAINT `portfolio_operations_transaction_id_fkey` FOREIGN KEY (`transaction_id`) REFERENCES `transactions`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `portfolio_operations` ADD CONSTRAINT `portfolio_operations_stock_id_fkey` FOREIGN KEY (`stock_id`) REFERENCES `stocks`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `portfolio_performance_history` ADD CONSTRAINT `portfolio_performance_history_account_id_fkey` FOREIGN KEY (`account_id`) REFERENCES `accounts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
